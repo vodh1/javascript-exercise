78db439484c9c7e149c59d90fe41027bb302668a
Andre Lee Cuyugan <andrelee.cuyugan@gmail.com>
2024-04-27 16:30:37 +0800

    feat: Finish forEachLoop.js activity

--- src/forEachLoop.js
+++ src/forEachLoop.js
@@ -6,3 +6,29 @@ Sample Input:
 Expected Output:
   150
 */
+// const numbers = [10,20,30,40,50]
+// let total = 0
+// let i =0
+// numbers.forEach((number)=> {
+//   total += number;
+//   console.log(number)
+// });
+
+// console.log(total);
+
+/*  
+ Exercise: 
+   Create an array of names and use the .forEach loop to print each name in uppercase.
+ Sample Input:  
+   ["Alice", "Bob", "Charlie"]
+ Expected Output:
+   ALICE
+   BOB
+   CHARLIE 
+*/
+
+const names = ["Alice", "Bob", "Charlie"]
+
+names.forEach((name) => {
+  console.log(name.toUpperCase());
+});
