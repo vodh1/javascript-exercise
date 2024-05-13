f7bd18d95fc59e2d702f05774dd21bc0602917b3
Andre Lee Cuyugan <andrelee.cuyugan@gmail.com>
2024-05-13 21:28:16 +0800

    feat: solve combinedExercised using .map, .reduce, or .forEach

--- src/combinedExercise.js
+++ src/combinedExercise.js
@@ -16,7 +16,36 @@ Sample Input:
       age: 28 
     } 
   ]
-Expected Outpus: 
+Expected Output: 
   Names: ["Alice", "Bob", "Charlie"]
   Average Age: 25
 */
+
+const persons = [
+  {
+    name: "Alice",
+    age: 25
+  },
+  {
+    name: "Bob",
+    age: 22
+  },
+  {
+    name: "Charlie",
+    age: 28
+  }
+]
+
+const names = persons.map(person => {
+  return person.name
+});
+
+console.log('Names: ', names)
+
+const total_age = persons.reduce((total_age, person) => {
+  return total_age += person.age
+}, 0)
+
+const average_age = total_age / persons.length
+
+console.log('Average Age: ', average_age)
